d0dfce64f03e738ce9bc6239d346b45d10f9add2
sana1202 <lksana1202@gmail.com>
2024-01-27 14:58:11 +0000

    commit from Sana

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,8 @@
 public class Test {
+    static void test()
+    {
+        System.out.println("Git changes test");
+    }
 
     public static void main(String[] args) {
         System.out.println("My name is Chetan");
